6117254a5dd753d6c23d175008cca5e5948fcfd6
kokomarlon727-hub <kokomarlon727@gmail.com>
2025-12-01 19:15:50 -0300

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,13 @@
+print "Digite seu nome: "
+
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+
+idade = gets.chomp.to_i
+
+print "Seu nome é #{nome} #{sobrenome} e sua idade é #{idade}."
